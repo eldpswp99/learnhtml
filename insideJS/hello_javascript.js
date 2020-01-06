c0c905de9a8a9819c869d2cc6f85d43c28cf0e00
eldpswp99 <nrg1392@naver.com>
2020-01-06 14:59:36 +0000

    ~closure

--- insideJS/hello_javascript.js
+++ insideJS/hello_javascript.js
@@ -1,20 +1,10 @@
-var foo = {
-	name : "foo",
-	age : 35,
-	gender : 'man'
-};
-
-console.dir(foo);
-
-function Person(name,age,gender,position){
-	this.name = name;
-	this.age = age;
-	this.gender = gender;
+function callLater(obj, a, b){
+	return (function(){
+		obj["sum"] = a+b;
+		console.log(obj["sum"]);
+	});
 }
 
-var qux = Person("qux",20,"man");
-console.log(qux);
-
-console.log(window.name);
-console.log(window.age);
-console.log(window.gender);
+var sumObj ={
+	
+}
